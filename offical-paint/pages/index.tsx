66cd22a30617fdb9275803235478ca165b927e5e
Carlos Uribe <carlos.uribe@orasi.com>
2022-11-28 11:13:13 -0500

    gkj

---
+++ offical-paint/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import {Homee} from '../components/Homee'
+
+const Home: NextPage = () => {
+  return (
+  <div>
+    <Homee />
+  </div>
+  )
+}
+
+export default Home
